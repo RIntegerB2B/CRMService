34a572b8fa2de80f8f813f1ac55e2b5b45d1d1b4
ripsildeveloper <ripsildeveloper3@gmail.com>
2018-12-30 20:51:56 +0530

    changes in image url

--- src/config/appSetting.js
+++ src/config/appSetting.js
@@ -1,6 +1,6 @@
 module.exports = {
     /* imageServerPath: 'http://localhost/ucchal/',
     imageUploadPath: './emailimage', */
-   imageServerPath: process.env.IMAGE_SERVER_PATH || 'http://ucchal.com.com/admin/images/',
+   imageServerPath: process.env.IMAGE_SERVER_PATH || 'http://ucchal.com.com/admin/crm/images/',
   imageUploadPath:process.env.IMAGE_UPLOAD_PATH || '/home/ubuntu/app/rinteger-crm/rinteger-crm/images' ,
 }
